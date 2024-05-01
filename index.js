0cccc49f60d2e44b39aa401a8c8c1c7fbc416f65
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2024-05-01 16:02:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); // Use built-in JSON parsing middleware
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "something" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
